660319f63870873964244486bf173bb12600c15d
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-10 20:28:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length == 2 
-  array [-2] << "and"
+  array [-2] << " and "
   array.join
   elsif array.length == 1 
     array.join
   elsif array.length == 3
-    array[-1].prepend "and"
+    array[-1].prepend "and "
     array.join (", ")
   end
 end
